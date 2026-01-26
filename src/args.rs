d42044aef0d78b9c3191c66f145544befb8c06a9
Johann Li <me@qinka.pro>
2026-01-26 23:05:05 +0800

    创建基本代码

---
+++ src/args.rs
@@ -0,0 +1,23 @@
+// 该文件是 Shanan （山南西风） 项目的一部分。
+// src/args.rs - 项目参数配置
+//
+// 本程序遵循 GNU Affero 通用公共许可证（AGPL）许可协议。
+// 本程序的发布旨在提供实用价值，但不作任何形式的担保，
+// 包括但不限于对适销性或特定用途适用性的默示担保。
+// 更多详情请参阅 GNU 通用公共许可证。
+//
+// Copyright (C) 2026 Johann Li <me@qinka.pro>, ETVP
+
+use clap::Parser;
+
+/// Shanan 项目参数配置
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+  /// RKNN 模型文件路径
+  #[arg(long, value_name = "FILE")]
+  pub model: String,
+  /// 输入来源
+  #[arg(long, value_name = "SOURCE")]
+  pub input: String,
+}
