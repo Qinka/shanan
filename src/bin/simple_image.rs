f4cf89191a90c8e7f8cb505d409009934e502299
Johann Li <me@qinka.pro>
2026-01-28 23:20:07 +0800

    修正测试代码

--- src/bin/simple_image.rs
+++ src/bin/simple_image.rs
@@ -1,5 +1,5 @@
 // 该文件是 Shanan （山南西风） 项目的一部分。
-// src/bin/simple.rs - 推理测试代码
+// src/bin/simple_image.rs - 简单的图像推理代码
 //
 // 本程序遵循 GNU Affero 通用公共许可证（AGPL）许可协议。
 // 本程序的发布旨在提供实用价值，但不作任何形式的担保，
@@ -14,7 +14,7 @@ use url::Url;
 
 use shanan::{
   FromUrl,
-  model::{CocoLabel, DetectResult, Model},
+  model::{CocoLabel, Model},
   output::Render,
 };
 use tracing::info;
@@ -47,10 +47,10 @@ fn main() -> Result<()> {
   let model = shanan::model::Yolo26Builder::from_url(&args.model)?.build()?;
   let output = shanan::output::SaveImageFileOutput::from_url(&args.output)?;
 
-  info!("开始推理...");
-  let now = std::time::Instant::now();
   for frame in input_image.into_nhwc() {
-    let result: DetectResult<CocoLabel> = model.infer(&frame)?;
+    info!("开始推理...");
+    let now = std::time::Instant::now();
+    let result: shanan::model::DetectResult<CocoLabel> = model.infer(&frame)?;
     let elapsed = now.elapsed();
     info!("推理完成，耗时: {:.2?}", elapsed);
     output.render_result(&frame, &result)?;
